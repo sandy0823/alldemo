3c43063ca8c224f225fbea04f9e2c5ae2bce94d0
sandy0823 <sandyflower@yeah.net>
2020-09-14 09:50:02 +0800

    [ADD]添加demo所有文件

---
+++ spring-boot-demo/src/main/java/com/example/demo/DemoApplication.java
@@ -0,0 +1,14 @@
+package com.example.demo;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+@MapperScan("com.example.demo.dao") // mybatis扫描路径，针对的是接口Mapper类
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+}
