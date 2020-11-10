22e55050ef37736a5f1bc8e3d4fd8d93008622ee
sandy0823 <sandyflower@yeah.net>
2020-11-10 20:31:33 +0800

    [ADD]添加 springboot-redis内容和对应的demo代码

---
+++ spring-family/springboot/springboot-demo/springboot-redis-demo/src/main/java/com/summer/demo/springboot/redis/RediesDemoController.java
@@ -0,0 +1,43 @@
+package com.summer.demo.springboot.redis;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class RediesDemoController {
+	@Autowired
+	private RedisTemplate<String, Object> redisTemplate;
+
+	@GetMapping("/jedis/hello")
+	public Object testJedis() {
+		Object value = redisTemplate.opsForValue().get("name");
+		if (value == null) {
+			redisTemplate.opsForValue().set("name", "jedis");
+			value = redisTemplate.opsForValue().get("name");
+		}
+		return value;
+	}
+
+	@GetMapping("/lettuce/hello")
+	public Object testLettuce() {
+		Object value = redisTemplate.opsForValue().get("name");
+		if (value == null) {
+			redisTemplate.opsForValue().set("name", "lettuce");
+			value = redisTemplate.opsForValue().get("name");
+		}
+		return value;
+	}
+
+	@GetMapping("/query/hello")
+	public Object testSection(@RequestParam("key") String key) {
+		Object value = redisTemplate.opsForValue().get(key);
+		if (value == null) {
+			redisTemplate.opsForValue().set(key, key);
+			value = redisTemplate.opsForValue().get(key);
+		}
+		return value;
+	}
+}
---
+++ spring-family/springboot/springboot-redis/src/main/java/com/summer/springboot/redis/config/LettuceRedisConfig.java
@@ -0,0 +1,85 @@
+package com.summer.springboot.redis.config;
+
+import java.time.Duration;
+
+import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingClass;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.boot.autoconfigure.data.redis.LettuceClientConfigurationBuilderCustomizer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.lettuce.LettuceClientConfiguration.LettuceClientConfigurationBuilder;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.JdkSerializationRedisSerializer;
+import org.springframework.data.redis.serializer.RedisSerializer;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+
+import io.lettuce.core.RedisClient;
+import io.lettuce.core.SocketOptions;
+import io.lettuce.core.cluster.ClusterClientOptions;
+import io.lettuce.core.cluster.ClusterTopologyRefreshOptions;
+
+/**
+ * 开启 lettuce 刷新redis 集群拓扑结构图的能力
+ * @author sandy
+ *
+ */
+@Configuration
+@ConditionalOnClass(RedisClient.class)
+public class LettuceRedisConfig{
+    /**
+     * RedisTemplate配置
+     * @param connectionFactory
+     * @return
+     */
+    @Bean
+    @ConditionalOnMissingBean(RedisTemplate.class)
+    public RedisTemplate<String, Object> redisTemplate(LettuceConnectionFactory connectionFactory) {
+        // 配置redisTemplate
+        RedisTemplate<String, Object> redisTemplate = new RedisTemplate<>();
+        redisTemplate.setConnectionFactory(connectionFactory);
+        RedisSerializer<String> redisKeySerializer = new StringRedisSerializer();
+        redisTemplate.setKeySerializer(redisKeySerializer);
+        redisTemplate.setHashKeySerializer(redisKeySerializer);
+        redisTemplate.setValueSerializer(new JdkSerializationRedisSerializer());
+        redisTemplate.setHashValueSerializer(new JdkSerializationRedisSerializer());
+        redisTemplate.afterPropertiesSet();
+        return redisTemplate;
+    }
+     
+@Bean
+@ConditionalOnProperty(name = "spring.redis.cluster.nodes")
+@ConditionalOnMissingBean(RedisConnectionFactory.class)
+@ConditionalOnMissingClass("org.springframework.boot.autoconfigure.data.redis.RedisProperties.Lettuce.Cluster")
+    public LettuceClientConfigurationBuilderCustomizer builderCustomizers(){
+        // 配置用于开启自适应刷新和定时刷新。如自适应刷新不开启，Redis集群变更时将会导致连接异常
+        ClusterTopologyRefreshOptions clusterTopologyRefreshOptions = ClusterTopologyRefreshOptions.builder()
+                .enablePeriodicRefresh(Duration.ofSeconds(30))// 开启周期刷新(默认60秒)
+                .enableAllAdaptiveRefreshTriggers() //开启自适应刷新
+                .enablePeriodicRefresh(true)
+                // 自适应刷新超时时间(默认30秒)
+                .adaptiveRefreshTriggersTimeout(Duration.ofSeconds(10)) //默认关闭开启后时间为30秒
+                .refreshTriggersReconnectAttempts(3)
+                .build();
+        ClusterClientOptions clusterClientOptions = ClusterClientOptions.builder()
+                                 .topologyRefreshOptions(clusterTopologyRefreshOptions)//拓扑刷新
+                              //   .disconnectedBehavior(ClientOptions.DisconnectedBehavior.REJECT_COMMANDS)
+                                 .autoReconnect(true)
+                                 .socketOptions(SocketOptions.builder().keepAlive(true).build())
+                                 .validateClusterNodeMembership(false)// 取消校验集群节点的成员关系
+                                 .build();
+         
+        LettuceClientConfigurationBuilderCustomizer customizer = new LettuceClientConfigurationBuilderCustomizer() {
+            @Override
+            public void customize(LettuceClientConfigurationBuilder clientConfigurationBuilder) {
+                clientConfigurationBuilder.clientOptions(clusterClientOptions);
+                //clientConfigurationBuilder.readFrom(ReadFrom.SLAVE_PREFERRED);
+            }
+        };
+         
+        return customizer;
+    }
+}
